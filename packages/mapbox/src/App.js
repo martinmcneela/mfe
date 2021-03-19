591c8017c81d3e15eb394d778701c578dedb03e8
Martin <mcneela86@gmail.com>
2021-03-19 18:49:04 +0000

    update mapbox

--- packages/mapbox/src/App.js
+++ packages/mapbox/src/App.js
@@ -5,10 +5,12 @@ import styled from "styled-components";
 const Title = styled.h1`
   text-decoration: underline;
   color: dodgerblue;
+  font-family: monospace;
 `;
 
 const ExternalLink = styled.a`
   color: palevioletred;
+  font-family: cursive;
   :hover {
     color: #009e69;
     text-decoration: none;
